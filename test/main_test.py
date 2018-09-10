f66abdc8ba2e55988d9fafbf89aac1da1103392d
Daniel Barra <dmbarra@gmail.com>
2018-09-10 09:07:39 -0300

    start manipulate response

--- test/main_test.py
+++ test/main_test.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from test.load_csv import LoadDataFile
 from test.rest_requests.browse_po import BrowsePo
 from ddt import ddt, idata
@@ -15,7 +16,12 @@ class Test(unittest.TestCase):
     def test_to_validate_po_configs(self, value):
         self.browsePo = BrowsePo(value[12], value[19])
         self.result = self.browsePo.connect_and_request_information()
+        self.validate_browse_po_response(self.result, value)
         print(value)
+
+    def validate_browse_po_response(self, response, value):
+        assert response.status_code == requests.codes.ok
+        assert response.json()
 
 
 if __name__ == "__main__":
